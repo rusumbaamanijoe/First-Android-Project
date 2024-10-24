fe2a5dc40ee7dd45f0b818e31f805d10899abcbb
Josue RUSUMBA <rusumbaamanijosue@gmail.com>
2024-10-24 10:44:41 +0300

    Commit #10
    The method on create that i have putted there will display everything that i declared i all my fragments (java class and layouts )

--- app/src/main/java/com/example/studentmanagementapp/MainActivity.java
+++ app/src/main/java/com/example/studentmanagementapp/MainActivity.java
@@ -1,24 +1,27 @@
 package com.example.studentmanagementapp;
 
-import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
+import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.lifecycle.ViewModelProvider;
 
 public class MainActivity extends AppCompatActivity {
+    private StudentViewModel studentViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Initialize the ViewModel
+        studentViewModel = new ViewModelProvider(this).get(StudentViewModel.class);
+
+        // Load fragments dynamically if they are not already loaded
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container_data_entry, new DataEntryFragment())
+                    .replace(R.id.fragment_container_display, new DisplayFragment())
+                    .commit();
+        }
     }
 }
