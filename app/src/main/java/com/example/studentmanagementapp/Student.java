32aff583fbe779c6eb8dfaf039063f1ffb10291d
Josue RUSUMBA <rusumbaamanijosue@gmail.com>
2024-10-24 10:33:07 +0300

    Revert "Commit #8"
    I have added four(4) getter methods of those variable that i have created which is getName, getAge, getGrade, getMajor

--- app/src/main/java/com/example/studentmanagementapp/Student.java
+++ app/src/main/java/com/example/studentmanagementapp/Student.java
@@ -1,4 +1,36 @@
 package com.example.studentmanagementapp;
 
 public class Student {
+    private final String name;
+    private final int age;
+    private final int grade;
+    private final String major;
+
+    public Student(String name, int age, int grade, String major) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+        this.major = major;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public int getAge() {
+
+        return age;
+    }
+
+    public int getGrade() {
+
+        return grade;
+    }
+
+    public String getMajor() {
+
+        return major;
+    }
+
 }
