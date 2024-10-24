9ca45b143cee9ee97f246c313ad14c531d49d9f1
Josue RUSUMBA <rusumbaamanijosue@gmail.com>
2024-10-24 09:54:50 +0300

    Commit #7
    I have added a method called onCreateView that will display the textViews i declared in my DisplayFragment.java class then i go to the fragment_display.xml file to create them and also add message to my strings.xml file to be displayed

--- app/src/main/java/com/example/studentmanagementapp/DisplayFragment.java
+++ app/src/main/java/com/example/studentmanagementapp/DisplayFragment.java
@@ -1,64 +1,41 @@
 package com.example.studentmanagementapp;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DisplayFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
 public class DisplayFragment extends Fragment {
+    private TextView textViewName, textViewAge, textViewGrade, textViewMajor;
+    private StudentViewModel studentViewModel;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public DisplayFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DisplayFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DisplayFragment newInstance(String param1, String param2) {
-        DisplayFragment fragment = new DisplayFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_display, container, false);
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_display, container, false);
+        textViewName = view.findViewById(R.id.textViewName);
+        textViewAge = view.findViewById(R.id.textViewAge);
+        textViewGrade = view.findViewById(R.id.textViewGrade);
+        textViewMajor = view.findViewById(R.id.textViewMajor);
+
+        studentViewModel = new ViewModelProvider(requireActivity()).get(StudentViewModel.class);
+        studentViewModel.getStudent().observe(getViewLifecycleOwner(), student -> {
+            if (student != null) {
+                textViewName.setText("Name: " + student.getName());
+                textViewAge.setText("Age: " + student.getAge());
+                textViewGrade.setText("Grade: " + student.getGrade());
+                textViewMajor.setText("Major: " + student.getMajor());
+            }
+        });
+
+        return view;
     }
 }
