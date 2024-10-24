2997afead183bf41cbb3c4fc27f28f82fa83dbe7
Josue RUSUMBA <rusumbaamanijosue@gmail.com>
2024-10-24 10:39:42 +0300

    Commit #9
    I created a method called getStudent() which that will be in other class to display the info

--- app/src/main/java/com/example/studentmanagementapp/StudentViewModel.java
+++ app/src/main/java/com/example/studentmanagementapp/StudentViewModel.java
@@ -1,4 +1,17 @@
 package com.example.studentmanagementapp;
 
-public class StudentViewModel {
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class StudentViewModel extends ViewModel {
+    private final MutableLiveData<Student> student = new MutableLiveData<>();
+
+    public void setStudent(Student newStudent) {
+        student.setValue(newStudent);
+    }
+
+    public LiveData<Student> getStudent() {
+        return student;
+    }
 }
