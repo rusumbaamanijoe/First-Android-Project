ab2ca0cad7933f0c4cc48d164cd3f45f6e97256f
Josue RUSUMBA <rusumbaamanijosue@gmail.com>
2024-10-24 10:00:54 +0300

    Commit #8
    In the Student.java class i created, i declare 4 private final variables "two Strings" and "Two integers" then i put the constructor of my class called Student

--- app/src/main/java/com/example/studentmanagementapp/Student.java
+++ app/src/main/java/com/example/studentmanagementapp/Student.java
@@ -1,4 +1,15 @@
 package com.example.studentmanagementapp;
 
 public class Student {
+    private final String name;
+    private final int age;
+    private final int grade;
+    private final String major;
+
+    public Student(String name, int age, int grade, String major) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+        this.major = major;
+    }
 }
